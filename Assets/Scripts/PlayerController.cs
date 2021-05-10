d2667768b30fec504c695c8d36442861e9c69b9b
Chase Anderson <anderson.chasew@gmail.com>
2021-05-10 16:02:15 -0600

    ball moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     // FixedUpdate is used for physics calculations
     void FixedUpdate() 
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement);
     }
 
     void OnMove(InputValue movementValue)
     {
         // Function body
-        Vector2 movementVector = movementValue.get<Vector2>();
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x * 10.0f;
+        movementY = movementVector.y * 10.0f;
     }
 }
