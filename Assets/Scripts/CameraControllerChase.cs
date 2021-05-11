3e265cc75d4878969524f7ef1c3aabb8c1d8c831
Chase Anderson <anderson.chasew@gmail.com>
2021-05-11 17:57:40 -0600

    camera controller

--- Assets/Scripts/CameraControllerChase.cs
+++ Assets/Scripts/CameraControllerChase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraControllerChase : MonoBehaviour
 {
     private GameObject player;
     private float offsetZ;
@@ -21,7 +21,10 @@ public class CameraController : MonoBehaviour
         // Transform playerTrans = player.GetComponent<Transform>();
         Vector3 playerPosition = player.transform.position;
 
+        // One note here, don't need to get own transform, can just use transform (see biff below)
         Transform cameraTransform = GetComponent<Transform>();
+
+        Transform biff = this.transform;
 
         cameraTransform.position = new Vector3(playerPosition.x, cameraTransform.position.y, playerPosition.z + this.offsetZ);
     }
